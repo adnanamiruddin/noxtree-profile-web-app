be323a6d4f00a88c1bbb5820149dfa2d388cb3ee
MrAdnan34 <adnan.amiruddin34@gmail.com>
2023-08-10 23:11:47 +0800

    fix: Implement component Input on Login page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,7 @@
 import { login } from "@/api/services";
 import { useState } from "react";
 import nookies from "nookies";
+import Input from "@/components/Input";
 
 export default function Login() {
   const [fields, setFields] = useState({
@@ -22,7 +23,7 @@ export default function Login() {
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
+        <div className="text-center lg:ml-12 lg:text-left">
           <h1 className="text-5xl font-bold">Login now!</h1>
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
@@ -33,41 +34,32 @@ export default function Login() {
 
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
           <form onSubmit={handleSubmit} className="card-body">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="text"
-                placeholder="email"
-                name="identifier"
-                onChange={handleFieldChange}
-                className="input input-bordered"
-              />
+            <div className="form-control gap-6">
+              <div>
+                <Input
+                  label="Email"
+                  isRequire
+                  name="identifier"
+                  placeholder="youremail99@email.com"
+                  handleChangeInput={handleFieldChange}
+                />
+              </div>
+
+              <div>
+                <Input
+                  type="password"
+                  label="Password"
+                  isRequire
+                  name="password"
+                  placeholder="yourpassword"
+                  handleChangeInput={handleFieldChange}
+                />
+              </div>
             </div>
 
-            <div className="divider"></div>
-
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="password"
-                placeholder="password"
-                name="password"
-                onChange={handleFieldChange}
-                className="input input-bordered"
-              />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
-            </div>
             <div className="form-control mt-6">
               <button className="btn btn-primary" type="submit">
-                Login
+                Register
               </button>
             </div>
           </form>
