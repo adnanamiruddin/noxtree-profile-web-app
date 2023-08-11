d2f54eb92c058f584eab93230f32b3a3a925f0b7
MrAdnan34 <adnan.amiruddin34@gmail.com>
2023-08-11 13:29:22 +0800

    fix: Add handling on Slug page if user don't have profile photo and any links

--- src/pages/[slug].jsx
+++ src/pages/[slug].jsx
@@ -29,13 +29,17 @@ export default function SlugPage({ data }) {
       className={`flex min-h-screen max-w-2xl m-auto flex-col items-center p-4 pt-24 ${inter.className}`}
     >
       <div className="relative w-[150px] h-[150px] rounded-full overflow-hidden mb-10">
-        <Image
-          src={`${process.env.NEXT_PUBLIC_ASSET_URL}${data.attributes.photo.data.attributes.url}`}
-          alt={data.attributes.fullname}
-          layout="fill"
-          objectFit="cover"
-          className="relative"
-        />
+        {data.attributes.photo?.data?.attributes?.url ? (
+          <Image
+            src={`${process.env.NEXT_PUBLIC_ASSET_URL}${data.attributes.photo.data.attributes.url}`}
+            alt={data.attributes.fullname}
+            layout="fill"
+            objectFit="cover"
+            className="relative"
+          />
+        ) : (
+          <div className="absolute w-full h-full bg-gray-300 rounded-full"></div>
+        )}
       </div>
 
       <div className="flex flex-col items-center gap-3 w-full mb-12">
@@ -44,47 +48,53 @@ export default function SlugPage({ data }) {
       </div>
 
       <div className="flex flex-col items-center w-full gap-8">
-        {accountLinks.map((value, index) => {
-          const linkStatus = value.attributes.status;
-          const isDeactive = linkStatus === "deactive";
-          const isSuspend = linkStatus === "suspend";
+        {accountLinks.length > 0 ? (
+          accountLinks.map((value, index) => {
+            const linkStatus = value.attributes.status;
+            const isDeactive = linkStatus === "deactive";
+            const isSuspend = linkStatus === "suspend";
 
-          if (isDeactive) return null;
+            if (isDeactive) return null;
 
-          return (
-            <a
-              key={index}
-              href={!isSuspend ? value.attributes.url : null}
-              target="_blank"
-              rel="noopener noreferrer"
-              className={`h-full w-full flex items-center gap-3 bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30 rounded-[20px] p-4  transition-all duration-300 ${
-                isSuspend ? "filter brightness-50" : ""
-              } ${!isSuspend ? "hover:scale-105" : ""} ${
-                !isSuspend ? "hover:cursor-pointer" : ""
-              }`}
-            >
-              <div className="w-[40px] h-[40px] rounded-full overflow-hidden">
-                <div
-                  className="w-full h-full relative"
-                  style={{ paddingBottom: "100%" }}
-                >
-                  {value.attributes.icon?.data?.attributes?.url ? (
-                    <Image
-                      src={`${process.env.NEXT_PUBLIC_ASSET_URL}${value.attributes.icon.data.attributes.url}`}
-                      alt={value.attributes.title}
-                      layout="fill"
-                      objectFit="cover"
-                      className="absolute rounded-full"
-                    />
-                  ) : (
-                    <div className="absolute w-12 h-10 bg-gray-300 rounded-full"></div>
-                  )}
+            return (
+              <a
+                key={index}
+                href={!isSuspend ? value.attributes.url : null}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={`h-full w-full flex items-center gap-3 bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30 rounded-[20px] p-4  transition-all duration-300 ${
+                  isSuspend ? "filter brightness-50" : ""
+                } ${!isSuspend ? "hover:scale-105" : ""} ${
+                  !isSuspend ? "hover:cursor-pointer" : ""
+                }`}
+              >
+                <div className="w-[40px] h-[40px] rounded-full overflow-hidden">
+                  <div
+                    className="w-full h-full relative"
+                    style={{ paddingBottom: "100%" }}
+                  >
+                    {value.attributes.icon?.data?.attributes?.url ? (
+                      <Image
+                        src={`${process.env.NEXT_PUBLIC_ASSET_URL}${value.attributes.icon.data.attributes.url}`}
+                        alt={value.attributes.title}
+                        layout="fill"
+                        objectFit="cover"
+                        className="absolute rounded-full"
+                      />
+                    ) : (
+                      <div className="absolute w-full h-full bg-gray-300 rounded-full"></div>
+                    )}
+                  </div>
                 </div>
-              </div>
-              <span className="text-lg">{value.attributes.title}</span>
-            </a>
-          );
-        })}
+                <span className="text-lg">{value.attributes.title}</span>
+              </a>
+            );
+          })
+        ) : (
+          <div className="flex items-center justify-center h-full w-full gap-3 bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30 rounded-[20px] p-4  transition-all duration-300">
+            <p className="text-2xl text-gray-400">No links available 😐</p>
+          </div>
+        )}
       </div>
     </main>
   );
